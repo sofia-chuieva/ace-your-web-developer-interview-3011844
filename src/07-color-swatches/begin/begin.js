c67fe34aed4004fabb62a458bf69fcbfa871aeb6
sschu <sschuyeva@gmail.com>
2023-02-28 21:05:23 +0000

    add colors task

--- src/07-color-swatches/begin/begin.js
+++ src/07-color-swatches/begin/begin.js
@@ -16,3 +16,39 @@ const swatchData = [
     rgb: "rgb(205, 218, 253)",
   },
 ];
+
+function constructSwatches(swatchData) {
+  const mainContainer = document.getElementById("swatch-container");
+
+  swatchData.forEach((item) => {
+    mainContainer.appendChild(create(item));
+  });
+}
+
+const create = (elem) => {
+  const cardElem = document.createElement("div");
+  cardElem.classList.add("card");
+
+  const colorElem = document.createElement("div");
+  colorElem.classList.add("card-color");
+  colorElem.style.backgroundColor = elem.hex;
+
+  const metaElem = document.createElement("div");
+  metaElem.classList.add("meta-data");
+
+  const hexParagraph = document.createElement("p");
+  hexParagraph.innerText = elem.hex;
+
+  const rgbParagraph = document.createElement("p");
+  rgbParagraph.innerText = elem.rgb;
+
+  metaElem.appendChild(hexParagraph);
+  metaElem.appendChild(rgbParagraph);
+
+  cardElem.appendChild(colorElem);
+  cardElem.appendChild(metaElem);
+
+  return cardElem;
+};
+
+constructSwatches(swatchData);
